bfe406f8c7069cf6974f78c008dadf9752648211
Uki141 <ishii.antinfo@gmail.com>
2019-11-18 08:12:55 +0900

    controllerに処理をまとめ始めたい

---
+++ TrelloLike/controller/login.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template, request
+from models.models import User
+from models.database import db_session
+
--- TrelloLike/models/models.py
+++ TrelloLike/models/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, Text, DateTime
 from models.database import Base
 from datetime import datetime
 
+
 class PostIt(Base):
     __tablename__ = 'postit'
     id = Column(Integer, primary_key=True)
@@ -14,3 +15,17 @@ class PostIt(Base):
 
     def __repr__(self):
         return f'<Title {self.title}>'
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), unique=True)
+    pw = Column(String(128))
+
+    def __init__(self, name=None, pw=None):
+        self.name = name,
+        self.pw = pw
+
+    def __repr__(self):
+        return f'<Name {self.name}>'
